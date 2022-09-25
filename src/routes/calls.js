9344a32c088bb986f7e41b4540a0a2d470e35f86
Haig (Hike) Hovsepian <hhovsepi@gmail.com>
2022-09-25 13:23:18 -0700

    documentation achieved through swagger js doc, also minor changes to functionality including recursive engineStartStop

--- src/routes/calls.js
+++ src/routes/calls.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import httpAdapter from 'axios/lib/adapters/http.js';
 
-import { invalidVehicleIdError, noBatteryError, noFuelError, invalidActionError, shouldBeGetError, shouldBePostError, invalidRouteError }  from '../middleware/errorHandler.js';
+import { invalidVehicleIdError, noBatteryError, noFuelError, invalidActionError, shouldBeGetError, shouldBePostError, invalidRouteError, apiError }  from '../middleware/errorHandler.js';
 
-axios.defaults.adapter = httpAdapter;
+axios.defaults.adapter = httpAdapter; // ** This is required for the tests to work because of the way axios is mocked ** //
 
 // ***** Get Vehicle Info ***** //
 export const getVehicleInfo = async (req, res) => {
@@ -16,16 +16,17 @@ export const getVehicleInfo = async (req, res) => {
   try {
     const response = await axios.post(url, data);
     if (response.data.status === '404') { return res.status(404).json(invalidVehicleIdError(id)); }
+     // ** If the vehicle ID is invalid, return an error
     const vehicleInfo = {
       vin: response.data.data.vin.value,
       color: response.data.data.color.value,
-      doorCount: response.data.data.fourDoorSedan.value === 'True' ? 4 : 2,
+      doorCount: response.data.data.fourDoorSedan.value === 'True' ? 4 : 2, // ** If fourDoorSedan is true, return 4, else it is a twoDoorCoupe and return 2
       driveTrain: response.data.data.driveTrain.value
     };
     return res.json(vehicleInfo);
   } catch (error) {
     console.error(error);
-    return res.json({ status: 'error', message: 'There was an error getting the vehicle info' });
+    return res.status(500).json(apiError(req.path));
   }
 };
 
@@ -40,16 +41,17 @@ export const getSecurityStatus = async (req, res) => {
   try {
     const response = await axios.post(url, data);
     if (response.data.status === '404') { return res.status(404).json(invalidVehicleIdError(id)); }
+    // ** If the vehicle ID is invalid, return an error
     const securityStatus = response.data.data.doors.values.map(door => {
       return {
         location: door.location.value,
-        locked: door.locked.value === 'True'
+        locked: door.locked.value === 'True' // ** If locked is true, return true, else return false
       };
     });
     return res.json(securityStatus);
   } catch (error) {
     console.error(error);
-    return res.json({ status: 'error', message: 'There was an error getting the security status' });
+    return res.status(500).json(apiError(req.path));
   }
 };
 
@@ -64,14 +66,16 @@ export const getFuelRange = async (req, res) => {
   try {
     const response = await axios.post(url, data);
     if (response.data.status === '404') { return res.status(404).json(invalidVehicleIdError(id)); }
+    // ** If the vehicle ID is invalid, return an error
     if (response.data.data.tankLevel.type === 'Null') { return res.status(406).json(noFuelError(id)); }
+    // ** If the vehicle does not have fuel, return an error
     const fuelRange = {
       percent: response.data.data.tankLevel.value
     };
     return res.json(fuelRange);
   } catch (error) {
     console.error(error);
-    return res.json({ status: 'error', message: 'There was an error getting the fuel range' });
+    return res.status(500).json(apiError(req.path));
   }
 };
 
@@ -86,53 +90,76 @@ export const getBatteryRange = async (req, res) => {
   try {
     const response = await axios.post(url, data);
     if (response.data.status === '404') { return res.status(404).json(invalidVehicleIdError(id)); }
+     // ** If the vehicle ID is invalid, return an error
     if (response.data.data.batteryLevel.type === 'Null') { return res.status(406).json(noBatteryError(id)); }
+    // ** If the vehicle has no battery, return an error
     const batteryRange = {
       percent: response.data.data.batteryLevel.value
     };
     return res.json(batteryRange);
   } catch (error) {
     console.error(error);
-    return res.json({ status: 'error', message: 'There was an error getting the battery range' });
+    return res.status(500).json(apiError(req.path));
   }
 };
 
 // ***** Start/Stop Engine ***** //
 export const startStopEngine = async (req, res) => {
   const { id } = req.params;
-  const { action } = req.body;
+  let { action } = req.body
+  // ** standardize 'start' and 'stop' if passed in with different casing
+  action = action.toUpperCase();
   const url = 'http://gmapi.azurewebsites.net/actionEngineService';
   const data = {
     id,
     command: action === 'START' ? 'START_VEHICLE' : 'STOP_VEHICLE',
     responseType: 'JSON'
-  };
-  if (action !== 'START' && action !== 'STOP') { return res.status(400).json(invalidActionError(action)); }
+  }; // ** The API expects the command to be in all caps
+
+  if (action !== 'START' && action !== 'STOP') { return res.status(400).json(invalidActionError(action)); } 
+  // ** The API only accepts START or STOP as the action, so if it's not one of those, return an error
   try {
     const response = await axios.post(url, data);
     if (response.data.status === '404') { return res.status(404).json(invalidVehicleIdError(id)); }
+    // ** If the vehicle ID is invalid, return an error
     const actionResult = {
       status: response.data.actionResult.status
     };
+    // ** some fancy recursion if the operation fails with boundaries so no infinite loop
+    let counter = 0;
+    if (actionResult.status === 'FAILED' && counter <= 5) {
+      const retry = async () => {
+        const retryResponse = await axios.post(url, data);
+        if (retryResponse.data.actionResult.status === 'EXECUTED') {
+          actionResult.status = retryResponse.data.actionResult.status;
+        } else {
+          counter += 1;
+          await retry();
+        }
+      };
+      await retry();
+    }
     return res.json(actionResult);
   } catch (error) {
     console.error(error);
-    return res.json({ status: 'error', message: 'There was an error starting/stopping the engine' });
+    return res.status(500).json(apiError(req.path));
   }
 };
 
 // ***** Invalid Route or Method ***** //
 export const invalidMethodOrRoute = (req, res) => {
+  // ** Regex is the cleanest way to match a route
   const postRoutes = /\/vehicles\/\d+\/engine/;
   const getRoutes = /\/vehicles\/\d+\/doors|\/vehicles\/\d+\/fuel|\/vehicles\/\d+\/battery|\/vehicles\/\d+/;
-  // ** Engine Start/Stop Must be POST ** //
+  // ** Engine Start/Stop Must be POST 
   if (req.path.match(postRoutes) && req.method !== 'POST') {
     return res.status(405).json(shouldBePostError(req.method, req.path));
   }
-  // ** All Other Routes Must be GET ** //
+  // ** All Other Routes Must be GET 
   if (req.path.match(getRoutes) && req.method !== 'GET') {
     return res.status(405).json(shouldBeGetError(req.method, req.path));
   }
+  // ** Invalid routes get the generic 404
   return res.status(404).json(invalidRouteError(req.originalUrl));
 };
 
--- src/routes/router.js
+++ src/routes/router.js
@@ -15,17 +15,18 @@ import {
 } from './calls.js';
 
 import { noVehicleIdError } from '../middleware/errorHandler.js';
+import welcome from './welcome.js';
 
-// TODO: !!! make an error specifically for if the calls themselves fail !!!
 
+// ** Swagger Config for Documentation ** //
 const router = express.Router();
 
 const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-      title: 'Vehicle API',
-      description: 'Vehicle API Information',
+      title: 'Smartcar API Wrapper for GM',
+      description: 'Documentation for the Smartcar API Wrapper for GM',
       contact: {
         name: 'Awesome Developer'
       },
@@ -41,7 +42,7 @@ router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 
 router.get('/', (req, res) => {
-  res.send({ message: 'Welcome to the Smartcar API!' });
+  res.status(200).json(welcome);
 });
 
 
@@ -55,7 +56,7 @@ router.get('/vehicles', (req, res) => {
  * /vehicles/{id}:
  *   get:
  *     tags:
- *       - vehicle
+ *       - Vehicle Details
  *     summary: Get vehicle info
  *     description: Get vehicle info for a given vehicle id including vin, color, doorCount, and driveTrain
  *     operationId: getVehicleInfo
@@ -98,17 +99,34 @@ router.get('/vehicles', (req, res) => {
  *             schema:
  *               type: object
  *               properties:
- *                 code:
- *                   type: integer
- *                   default: 404
- *                   description: invalid vehicle ID provided
- *                 info:
+ *                 message:
  *                   type: string
- *                   default: Vehicle ID {ID} is invalid, please check your vehicle ID and try again.
- *                 type:
+ *                   default: Not Found
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     code:
+ *                       type: integer
+ *                       default: 404
+ *                       description: invalid vehicle ID provided
+ *                     info:
+ *                       type: string
+ *                       default: Vehicle ID {ID} is invalid, please check your vehicle ID and try again.
+ *                     type:
+ *                       type: string
+ *                       default: INVALID_VEHICLE_ID
+ *                       description: The drive train of the vehicle
+ *                 timestamp:
  *                   type: string
- *                   default: INVALID_VEHICLE_ID
- *                   description: The drive train of the vehicle
+ *                   default: 2021-03-01T00:00:00.000Z
+ *                   description: The timestamp of the request
+ *                 possibleRoutes:
+ *                   type: array
+ *                   items:
+ *                     type: string
  * 
  *     
  */
@@ -118,16 +136,296 @@ router.get('/vehicles/:id', async (req, res) => {
 
 
 // ***** Get Security Status ***** //
+/**
+ * @swagger
+ * /vehicles/{id}/doors:
+ *   get:
+ *     tags:
+ *       - Vehicle Security
+ *     summary: Get security status of vehicle doors
+ *     description: Get security status of vehicle doors for a given vehicle id including locked status of each door
+ *     operationId: getVehicleDoors
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: The vehicle id of the vehicle to get
+ *         required: true
+ *         schema:
+ *           type: string
+ *           default: 1234
+ *           enum:
+ *             - 1234
+ *             - 1235
+ *             - 123555555
+ *     responses:
+ *       '200':
+ *         description: successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   location:
+ *                     type: string
+ *                     default: frontLeft
+ *                     description: The color of the vehicle
+ *                   locked:
+ *                     type: boolean
+ *       '404':
+ *         description: Invalid vehicle ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   default: Not Found
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     code:
+ *                       type: integer
+ *                       default: 404
+ *                       description: invalid vehicle ID provided
+ *                     info:
+ *                       type: string
+ *                       default: Vehicle ID {ID} is invalid, please check your vehicle ID and try again.
+ *                     type:
+ *                       type: string
+ *                       default: INVALID_VEHICLE_ID
+ *                       description: The drive train of the vehicle
+ *                 timestamp:
+ *                   type: string
+ *                   default: 2021-03-01T00:00:00.000Z
+ *                   description: The timestamp of the request
+ *                 possibleRoutes:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ * 
+ *     
+ */
 router.get('/vehicles/:id/doors', async (req, res) => {
   await getSecurityStatus(req, res);
 });
 
-
+/**
+ * @swagger
+ * /vehicles/{id}/fuel:
+ *   get:
+ *     tags:
+ *       - Vehicle Fuel
+ *     summary: Get the fuel level of the vehicle
+ *     description: Get the fuel level of the vehicle for a given vehicle id as a percentage
+ *     operationId: getVehicleFuel
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: The vehicle id of the vehicle to get
+ *         required: true
+ *         schema:
+ *           type: string
+ *           default: 1234
+ *           enum:
+ *             - 1234
+ *             - 1235
+ *             - 123555555
+ *     responses:
+ *       '200':
+ *         description: successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 percent:
+ *                   type: string
+ *                   description: The remaining fuel level of the vehicle as a percentage
+ *                   default: 27.38
+ *       '404':
+ *         description: Invalid vehicle ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   default: Not Found
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     code:
+ *                       type: integer
+ *                       default: 404
+ *                       description: invalid vehicle ID provided
+ *                     info:
+ *                       type: string
+ *                       default: Vehicle ID {ID} is invalid, please check your vehicle ID and try again.
+ *                     type:
+ *                       type: string
+ *                       default: INVALID_VEHICLE_ID
+ *                       description: The drive train of the vehicle
+ *                 timestamp:
+ *                   type: string
+ *                   default: 2021-03-01T00:00:00.000Z
+ *                   description: The timestamp of the request
+ *                 possibleRoutes:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *       '406':
+ *         description: Vehicle does not use fuel
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   default: Not Acceptable
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     code:
+ *                       type: integer
+ *                       default: 406
+ *                       description: vehicle energy type is invalid
+ *                     info:
+ *                       type: string
+ *                       default: Vehicle ID 1235 does not use fuel. Path /fuel is only valid for gas or hybrid vehicles. Did you mean /battery?
+ *                     type:
+ *                       type: string
+ *                       default: NO_FUEL
+ *                 timestamp:
+ *                   type: string
+ *                   default: 2021-03-01T00:00:00.000Z
+ *                   description: The timestamp of the request
+ *                 possibleRoutes:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ */
 // ***** Get Fuel Range ***** //
 router.get('/vehicles/:id/fuel', async (req, res) => {
   await getFuelRange(req, res);
 });
 
+/**
+ * @swagger
+ * /vehicles/{id}/battery:
+ *   get:
+ *     tags:
+ *       - Vehicle Battery
+ *     summary: Get the battery level of the vehicle
+ *     description: Get the battery level of the vehicle for a given vehicle id as a percentage
+ *     operationId: getVehicleBattery
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: The vehicle id of the vehicle to get
+ *         required: true
+ *         schema:
+ *           type: string
+ *           default: 1234
+ *           enum:
+ *             - 1234
+ *             - 1235
+ *             - 123555555
+ *     responses:
+ *       '200':
+ *         description: successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 percent:
+ *                   type: string
+ *                   description: The remaining battery level of the vehicle as a percentage
+ *                   default: 27.39
+ *       '404':
+ *         description: Invalid vehicle ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   default: Not Found
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     code:
+ *                       type: integer
+ *                       default: 404
+ *                       description: invalid vehicle ID provided
+ *                     info:
+ *                       type: string
+ *                       default: Vehicle ID {ID} is invalid, please check your vehicle ID and try again.
+ *                     type:
+ *                       type: string
+ *                       default: INVALID_VEHICLE_ID
+ *                       description: The drive train of the vehicle
+ *                 timestamp:
+ *                   type: string
+ *                   default: 2021-03-01T00:00:00.000Z
+ *                   description: The timestamp of the request
+ *                 possibleRoutes:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *       '406':
+ *         description: Vehicle does not use battery
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   default: Not Acceptable
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     code:
+ *                       type: integer
+ *                       default: 406
+ *                       description: vehicle energy type is invalid
+ *                     info:
+ *                       type: string
+ *                       default: Vehicle ID 1234 does not use battery. Path /battery is only valid for electric or hybrid vehicles. Did you mean /fuel?
+ *                     type:
+ *                       type: string
+ *                       default: NO_BATTERY
+ *                 timestamp:
+ *                   type: string
+ *                   default: 2021-03-01T00:00:00.000Z
+ *                   description: The timestamp of the request
+ *                 possibleRoutes:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ */
 // ***** Get Battery Range ***** //
 router.get('/vehicles/:id/battery', async (req, res) => {
   await getBatteryRange(req, res);
@@ -135,6 +433,123 @@ router.get('/vehicles/:id/battery', async (req, res) => {
 
 
 // ***** Start/Stop Engine ***** //
+/**
+ * @swagger
+ * /vehicles/{id}/engine:
+ *   post:
+ *     tags:
+ *       - Vehicle Remote Start/Stop
+ *     summary: Start or stop the engine of the vehicle
+ *     description: Start or stop the engine of the vehicle for a given vehicle id
+ *     operationId: startStopVehicleEngine
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: The vehicle id of the vehicle to get
+ *         required: true
+ *         schema:
+ *           type: string
+ *           default: 1234
+ *           enum:
+ *             - 1234
+ *             - 1235
+ *             - 123555555
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               action:
+ *                 type: string
+ *                 description: The action to perform on the vehicle engine
+ *                 default: START
+ *                 enum:
+ *                   - START
+ *                   - STOP
+ *                   - GO FAST GO FURIOUS
+ *     responses:
+ *       '200':
+ *         description: successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   description: The execution status of the action
+ *                   default: EXECUTED
+ *       '404':
+ *         description: Invalid vehicle ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   default: Not Found
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     code:
+ *                       type: integer
+ *                       default: 404
+ *                       description: invalid vehicle ID provided
+ *                     info:
+ *                       type: string
+ *                       default: Vehicle ID {ID} is invalid, please check your vehicle ID and try again.
+ *                     type:
+ *                       type: string
+ *                       default: INVALID_VEHICLE_ID
+ *                       description: The drive train of the vehicle
+ *                 timestamp:
+ *                   type: string
+ *                   default: 2021-03-01T00:00:00.000Z
+ *                   description: The timestamp of the request
+ *                 possibleRoutes:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *       '405':
+ *         description: method not allowed
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   default: Method Not Allowed
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     code:
+ *                       type: integer
+ *                       default: 405
+ *                       description: invalid action provided
+ *                     info:
+ *                       type: string
+ *                       default: Method {method} is invalid for /vehicles/1234/engine - allowed methods are POST.
+ *                     type:
+ *                       type: string
+ *                       default: INVALID_METHOD
+ *                 timestamp:
+ *                   type: string
+ *                   default: 2021-03-01T00:00:00.000Z
+ *                   description: The timestamp of the request
+ *                 possibleRoutes:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ */
 router.post('/vehicles/:id/engine', async (req, res) => {
   await startStopEngine(req, res);
 });
